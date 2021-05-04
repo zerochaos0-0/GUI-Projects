7ed5f144732237a24c9c38916303d3d3883476a0
zerochaos0-0 <79869240+zerochaos0-0@users.noreply.github.com>
2021-05-04 11:49:54 -0600

    Add files via upload

--- tkinterTest.py
+++ tkinterTest.py
@@ -1,14 +1,37 @@
 from tkinter import *
 
 top = Tk()
+groceryList = []
 
-L1 = Label(top, text="Hello, world!")
+def results():
+    results = E1.get()
+    print(results)
+    print(type(results))
 
+def addToList():
+    newItem = E1.get()
+    groceryList.append(newItem)
+
+def clearList():
+    groceryList.clear()
+    
+
+#This is a label widget
+L1 = Label(top, text = "Grocery List")
 L1.grid(column = 0, row = 1)
 
+#This is an entry widget
 E1 = Entry(top, bd = 5)
-
 E1.grid(column = 0, row = 2)
+
+
+#This is a button widget
+B1 = Button(text = "    Print Your List    ", bg = "light blue", command = results)
+B1.grid(column = 1, row = 3)
+B2 = Button(text = "    Add to List    ", bg = "light blue", command = addToList)
+B2.grid(column = 1, row = 2)
+B3 = Button(text = "    Clear List    ", bg = "light blue", command = clearList)
+B3.grid(column = 0, row = 3)
 
 top.mainloop()
 
